123e597ab91da05f0d5b7541acb44e238eefa70b
Rzkyjlnsyh <suryasadewa2904@gmail.com>
2026-03-18 13:59:10 +0700

    Fix API Key Supabase zervdttmbpenbujkjcrn

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 // Pastikan env variables terisi, jika tidak kita panggil manual stringnya
 // Tetapi sangat disarankan menggunakan .env.local untuk keamanan
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://euxinsbjfukszxzejbop.supabase.co';
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV1eGluc2JqZnVrc3p4emVqYm9wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYyMTU0ODEsImV4cCI6MjA4MTc5MTQ4MX0.-ROOHd6Ou4XR7_4T_IPZir6b45dFKqfl6an9G37Cnvk';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://zervdttmbpenbujkjcrn.supabase.co';
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InplcnZkdHRtYnBlbmJ1amtqY3JuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM4MDk3MDksImV4cCI6MjA4OTM4NTcwOX0.CaIcoSbA_DQkWg-RNUA4KHj-1wlEL2OUCfnuYLb51gc';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
